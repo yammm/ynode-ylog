fba782d228c570e3a3c102d60c5b966c7751aa17
Michael Welter <me@mikinho.com>
2026-08-22 16:22:56 -0500

    style(license): align documentation generator year
    Signed-off-by: Michael Welter <me@mikinho.com>

--- scripts/gen-docs.mjs
+++ scripts/gen-docs.mjs
@@ -3,7 +3,7 @@
 /*
 The MIT License (MIT)
 
-Copyright (c) 2025 Michael Welter <me@mikinho.com>
+Copyright (c) 2026 Michael Welter <me@mikinho.com>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of
 this software and associated documentation files (the "Software"), to deal in
